3eb5a5f066c4518a46e829828f1f8ebf42c4dd4b
gnpjb <guido_gucci@hotmail.com>
2018-02-22 17:25:26 -0300

    created the blockRow class(unfinished)

--- file.h
+++ file.h
@@ -1,9 +1,14 @@
-#include<string>
-#include<fstream>
-#include<iostream>
-#include<vector>
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_ttf.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <time.h> 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+
+using namespace std;
 
 class Block{
 	private:
@@ -13,9 +18,9 @@ class Block{
 		int h;
 		int lives;
 	public:
-		void SetPosition(int t_x,int t_y);
-		void SetSize(int t_w,int t_h);
-		void SetLives(int t_lives);
+		void SetPosition(int t_x,int t_y);//set position. -1 to not change it
+		void SetSize(int t_w,int t_h);//set size. -1 to not change it
+		void SetLives(int t_lives);//set lives. -1 to not change it
 		void GetPosition(int* r_x,int* r_y);//can handle null
 		void GetSize(int* r_w,int* r_h);//can handle null
 		void GetLives(int* r_lives);//can handle null
@@ -66,3 +71,52 @@ void Block::GetLives(int* r_lives){
 	}
 }
 
+
+class BlockRow{
+private:
+	vector<Block> BlockVector(0);
+	int Lives;
+	int NumOfCol;
+	int Width;
+	int Height;
+	int yPos;
+public:
+	BlockRow();
+	void DownRow();
+	void DeleteBlock(int PositionInVector);
+};
+
+BlockRow::BlockRow(){
+	Block NewBlock;//block that will be added
+	NewBlock.SetSize(Width,Height);
+	NewBlock.SetLives(Lives);
+	bool WillBeAdded;
+	
+	
+	for(int i=0;i<NumOfCol;i++){
+		srand(time(NULL));
+		WillBeAdded=rand() % 2;
+		if(WillBeAdded){
+			NewBlock.SetPosition(i*(Widht/NumOfCol),yPos);
+			BlockVector.push_back(NewBlock);
+		}
+	}
+	
+	if(BlockVector.empty()){
+		srand(time(NULL));
+		int RandomNumber=rand()% NumOfCol;
+		NewBlock.SetPosition(RandomNumber*(Width/NumOfCol),yPos);
+	}
+}
+void BlockRow::DownRow(){
+	yPos=yPos-Height;
+	for(int i=0;i<BlockVector.size();i++){
+		(BlockVector[i]).SetPosition(-1,yPos);
+	}
+}
+
+void BlockRow::DeleteBlock(int PositionInVector){
+	
+	
+	
+}
